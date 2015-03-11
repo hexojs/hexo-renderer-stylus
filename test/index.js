1e9bf4c527fd3c98c9396fbdda70d9c99554f92c
Tommy Chen <tommy351@gmail.com>
2015-03-11 19:10:54 +0800

    Improve "hexo-config" method

--- test/index.js
+++ test/index.js
@@ -19,34 +19,18 @@ describe('Stylus renderer', function(){
 
   var r = require('../lib/renderer').bind(ctx);
 
-  var body = [
-    '.foo',
-    '  color: red',
-    '  content: hexo-config("foo")',
-    '',
-    '.bar',
-    '  content: hexo-config("bar.baz")',
-    '',
-    '.baz',
-    '  content: hexo-config("bar[baz]")'
-  ].join('\n');
-
   it('default', function(){
-    ctx.config.stylus.compress = false;
+    var body = [
+      '.foo',
+      '  color: red'
+    ].join('\n');
 
     r({text: body}, {}, function(err, result){
-      should.not.exist(err);
+      if (err) throw err;
 
       result.should.eql([
         '.foo {',
         '  color: #f00;',
-        '  content: 1;',
-        '}',
-        '.bar {',
-        '  content: 2;',
-        '}',
-        '.baz {',
-        '  content: 2;',
         '}'
       ].join('\n') + '\n');
     });
@@ -55,9 +39,71 @@ describe('Stylus renderer', function(){
   it('compress', function(){
     ctx.config.stylus.compress = true;
 
+    var body = [
+      '.foo',
+      '  color: red'
+    ].join('\n');
+
     r({text: body}, {}, function(err, result){
-      should.not.exist(err);
-      result.should.eql('.foo{color:#f00;content:1}.bar{content:2}.baz{content:2}');
+      if (err) throw err;
+
+      ctx.config.stylus.compress = false;
+      result.should.eql('.foo{color:#f00}');
+    });
+  });
+
+  it('hexo-config', function(){
+    var body = [
+      // first depth and exist
+      '.foo',
+      '  content: hexo-config("foo")',
+      '',
+      // second depth and exist
+      '.bar',
+      '  content: hexo-config("bar.baz")',
+      '',
+      // another style for nested attribute
+      '.baz',
+      '  content: hexo-config("bar[baz]")',
+      '',
+      // nested attribute does not exist
+      '.boo',
+      '  content: hexo-config("bar.boo")',
+      '',
+      // config does not exist
+      '.test',
+      '  content: hexo-config("boo")',
+      '',
+      // try to get nested attribute in non-object
+      '.app',
+      '  content: hexo-config("foo.test")'
+    ].join('\n');
+
+    r({text: body}, {}, function(err, result){
+      if (err) throw err;
+
+      console.log(result);
+
+      result.should.eql([
+        '.foo {',
+        '  content: 1;',
+        '}',
+        '.bar {',
+        '  content: 2;',
+        '}',
+        '.baz {',
+        '  content: 2;',
+        '}',
+        '.boo {',
+        '  content: \'\';',
+        '}',
+        '.test {',
+        '  content: \'\';',
+        '}',
+        '.app {',
+        '  content: \'\';',
+        '}'
+      ].join('\n') + '\n');
     });
   });
 });
